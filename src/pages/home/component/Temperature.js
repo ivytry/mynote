5e3ca8cd144bfa9a81cb74357fc8a1f71055b719
ivy <526560484@qq.com>
2021-05-10 11:04:43 +0800

    add temperture doc

--- src/pages/home/component/Temperature.js
+++ src/pages/home/component/Temperature.js
@@ -24,7 +24,7 @@ class Temperature extends Component {
 
 		let option = {
 		    title: {
-		        text: '近一月基础体温变化图',
+		        text: '基础体温变化图',
 		        subtext: temperture.peired,
 		        textStyle:{
 		        	color: '#969696',
@@ -76,6 +76,7 @@ class Temperature extends Component {
 		    },
 		    yAxis: {
 		        type: 'value',
+		        splitNumber:10,
 		        axisLabel: {
 		            formatter: '{value} ℃'
 		        },
@@ -117,12 +118,40 @@ class Temperature extends Component {
                 		}, {
                     		xAxis: '2021/4/6'
                 		}] ]
-            		}
+            		},
+		            markPoint: {
+		                data: [
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 5,
+		                    	coord: ["2021/4/3", 37.05]
+		                    },
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 5,
+		                    	coord: ["2021/4/4", 36.95]
+		                    },
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 3,
+		                    	coord: ["2021/4/5", 36.95]
+		                    },
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 3,
+		                    	coord: ["2021/4/6", 36.95]
+		                    }
+		                ]
+		            }
 		        },
 		        {
 		        	name:'温度计2',
 		            type:'line',
-		            data: temperture.temperture2,    
+		            data: temperture.temperture2,
 		            markPoint: {
 		                data: [
 		                    {
@@ -144,13 +173,43 @@ class Temperature extends Component {
 		           				itemStyle: {
 		           					color:'#1fad25'
 		           				}
-		                    }//,
+		                    },
 		                    //{
 		                    //	name: '爱爱',
 		                    //	symbol:'path//M512 919.194002l-64.352657-58.361199C219.085764 653.353378 68.191078 516.438079 68.191078 348.900342c0-136.915299 107.180068-244.094344 244.094344-244.094344 77.222779 0 151.3388 35.948747 199.713554 92.53451 48.374754-56.585763 122.490775-92.53451 199.713554-92.53451 136.915299 0 244.094344 107.180068 244.094344 244.094344 0 167.537737-150.894685 304.453037-379.456265 511.933485L512 919.194002z',
 		                    //	symbolSize:30,
         					//	coord: ["2021/4/25", 37]
-		                    //}
+		                    //},
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 20,
+		                    	coord: ["2021/5/6",  36.40]
+		                    },
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 10,
+		                    	coord: ["2021/5/7", 36.53]
+		                    },
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 5,
+		                    	coord: ["2021/5/8", 36.50]
+		                    },
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 3,
+		                    	coord: ["2021/5/9", 36.50]
+		                    },
+		                    {
+		                    	name: '经期',
+		                    	symbol: 'circle',
+		                    	symbolSize: 3,
+		                    	coord: ["2021/5/10", 36.50]
+		                    }
 		                ]
 		            },
             		markLine: {
